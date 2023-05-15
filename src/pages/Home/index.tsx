751cab338c6bec927697af396365c76e2eb695f3
Andrei Ce <andrei90@gmail.com>
2023-05-15 12:07:28 -0300

    design mostly done; next: functionality

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,9 @@
 import { Play } from '@phosphor-icons/react'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+// no efault exports:
+import * as zod from 'zod'
+
 import {
   CountDownContainer,
   FormContainer,
@@ -9,10 +14,38 @@ import {
   TaskInput,
 } from './styles'
 
+const newCycleValidationSchema = zod.object({
+  task: zod.string().min(1, { message: 'Please define the task name' }),
+  minutesSet: zod
+    .number()
+    .min(5, { message: 'Please set a min of 5 minutes' })
+    .max(60, { message: 'Set a min of 1 hour' }),
+})
+
+// 1) interfaces are generally to define objects, types are to infer
+// 2) whwnever Iwant to refer to a JS variable in a TS functionality, use topeof
+type NewCycleFormData = zod.infer<typeof newCycleValidationSchema>
+
 export function Home() {
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleValidationSchema),
+    defaultValues: { task: '', minutesSet: 0 },
+  })
+
+  function handleCreateNewCycle(data: any) {
+    console.log(data)
+    reset()
+  }
+
+  const task = watch('task')
+  const isSubmitButtonDisabled = !task
+
+  // destructure formState from useForm to use it below:
+  // console.log(formState.errors)
+
   return (
     <HomeContainer>
-      <form action="">
+      <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
         <FormContainer>
           <label htmlFor="task">I&apos;ll work on</label>
           <TaskInput
@@ -20,6 +53,8 @@ export function Home() {
             type="text"
             list="task-suggestions"
             placeholder="Give a name to your task"
+            // this register function will return other functions like onChange, etc.
+            {...register('task')}
           />
           <datalist id="task-suggestions">
             <option value="Project 155" />
@@ -36,6 +71,10 @@ export function Home() {
             max={60}
             id="minutesSet"
             placeholder="00"
+            {...register('minutesSet', {
+              required: true,
+              valueAsNumber: true,
+            })}
           />
 
           <span>minutes</span>
@@ -49,7 +88,7 @@ export function Home() {
           <span>0</span>
         </CountDownContainer>
 
-        <StartButton disabled type="submit">
+        <StartButton disabled={isSubmitButtonDisabled} type="submit">
           <Play size={24} />
           Start
         </StartButton>
